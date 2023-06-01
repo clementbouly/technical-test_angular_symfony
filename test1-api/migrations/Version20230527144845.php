e7290d686e6693fd5c59f4b3a6cedae724c3e79e
clementbouly <clementbouly@hotmail.fr>
2023-06-01 12:24:53 +0200

    core(docker): FAIL, try configure docker compose

--- test1-api/migrations/Version20230527144845.php
+++ test1-api/migrations/Version20230527144845.php
@@ -20,14 +20,14 @@ final class Version20230527144845 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, country VARCHAR(255) NOT NULL, year INT NOT NULL, original_title VARCHAR(255) DEFAULT NULL, description VARCHAR(255) DEFAULT NULL, genre VARCHAR(50) NOT NULL, ranking INT NOT NULL, price DOUBLE PRECISION NOT NULL, product_type VARCHAR(30) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE SEQUENCE product_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE product (id INT NOT NULL, title VARCHAR(255) NOT NULL, country VARCHAR(255) NOT NULL, year INT NOT NULL, original_title VARCHAR(255) DEFAULT NULL, description VARCHAR(255) DEFAULT NULL, genre VARCHAR(50) NOT NULL, ranking INT NOT NULL, price DOUBLE PRECISION NOT NULL, product_type VARCHAR(30) NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE product');
-        $this->addSql('DROP TABLE messenger_messages');
+        $this->addSql('DROP SEQUENCE product_id_seq CASCADE');
     }
 }
